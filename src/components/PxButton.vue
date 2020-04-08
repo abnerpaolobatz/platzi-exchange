7a77959eef88a8da99c2591012f307faa15d7324
Batz <batza@licorera.local>
2020-04-08 12:59:19 -0600

    project kick off

---
+++ src/components/PxButton.vue
@@ -0,0 +1,20 @@
+<template>
+    <button
+        @click="buttonClick"
+        class="bg_transparente hover:bg-green-500 text-green-700 font-semicolon"
+    >
+        <slot> </slot>
+    </button>
+</template>
+
+<script>
+export default {
+    name: 'PxButton',
+
+    methods: {
+        buttonClick() {
+            this.$emit('click')
+        }
+    }
+}
+</script>
